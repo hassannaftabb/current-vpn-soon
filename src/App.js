78b4d2f6b62ac4ebe783f3cc6d4cf7470fbee6e9
Hassan <hassannaftabb@gmail.com>
2023-12-13 00:13:38 +0500

    app added

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <img src="/logo.png" alt="Logo" />
+      </nav>
+
+      <main>
+        <img src="/soon.png" alt="Centered" />
+      </main>
+    </div>
+  );
+}
+
+export default App;
